dd8666cff4d04adbde31027dc59c9ad8c27abc4e
Ruan de Mattos Debossan <32501119+heatzn@users.noreply.github.com>
2023-09-30 19:29:52 -0300

    Add files via upload

---
+++ calc.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    while (true)
+    {
+        int resp;
+        int anoNasc;
+        int mesNasc;
+        int diaNasc;
+
+        cout << "=====================" << endl;
+        cout << "=====CALCULADORA=====" << endl;
+        cout << "=====================" << endl;
+        cout << "Funções:" << endl;
+        cout << "[1] Calcular idade" << endl;
+        cout << "[2] Conversor de medidas de tempo" << endl;
+        cout << "[3] Conversor de medidas de temperatura" << endl;
+        cout << "Escolha uma opção (1, 2, 3): \n";
+        cout << "> ";
+
+        cin >> resp;
+
+        switch (resp)
+        {
+        case 1:
+            cout << "Opção 1 selecionada" << endl;
+            cout << "Digite o dia de seu nascimento: " << endl;
+            cin >> diaNasc;
+
+            if (diaNasc < 32 && diaNasc > 0) {
+                cout << "Digite o mes de seu nascimento: " << endl;
+                cin >> mesNasc;
+
+                if (mesNasc < 13 && mesNasc > 0) {
+                    cout << "Digite o ano de seu nascimento: " << endl;
+                    cin >> anoNasc;
+
+                    if (anoNasc > 0 && anoNasc < 2023) {
+
+                        int aIdade = 2023 - anoNasc;
+                        int mIdade = aIdade * 12;
+                        int weekIdade = mIdade * 4;
+                        int dIdade = mIdade * 30;
+                        int hourIdade = dIdade * 24;
+                        int minIdade = hourIdade * 60;
+                        int secIdade = mIdade * 60;
+
+                        cout << "==========Sumário==========" << endl;
+                        cout << "Anos de idade: " << aIdade << endl;
+                        cout << "Meses: " << mIdade << endl;
+                        cout << "Dias: " << dIdade << endl;
+                        cout << "Semanas: " << weekIdade << endl;
+                        cout << "Horas: " << hourIdade << endl;
+                        cout << "Minutos/Segundos: " << minIdade << "m" << secIdade << "s\n";
+                        cout << "\n";
+                        cout << "\n";
+                    }
+                
+                }
+            }
+            else {
+                cout << "Erro, dado inválido" << endl;
+            }
+
+            break;
+               
+
+        case 2:
+            cout << "Opção 2 selecionada" << endl;
+            break;
+
+        case 3:
+            cout << "Opção 3 selecionada" << endl;
+            break;
+
+        default:
+            cout << "Opção inválida, tente novamente." << endl;
+            break;        
+        }
+    }
+    }
+
+    
